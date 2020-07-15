419403634b6f479589ea75dfdd5ae3dbbaaf012f
Guojin Tseng <guojintseng@gmail.com>
2020-07-15 23:17:34 +0800

    initial xcode project creation

---
+++ RouteBook-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RouteBook-ios
+//
+//  Created by Guojin Tseng on 7/15/20.
+//  Copyright © 2020 Guojin Tseng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
